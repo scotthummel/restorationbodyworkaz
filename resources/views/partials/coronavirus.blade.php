8d06522391036f1a52d05d982249b711b25ca256
Scott Hummel <scotthummel@icloud.com>
2020-06-09 10:58:36 -0700

    Coronavirus update 6/9

--- resources/views/partials/coronavirus.blade.php
+++ resources/views/partials/coronavirus.blade.php
@@ -1,7 +1,6 @@
 <div class="coronavirus desktop">
-    <h1>5/28 Update:</h1>
+    <h1>6/9 Update:</h1>
     <p>At the beginning of the global health crisis, the Arizona Board of Massage Therapy and Associated Bodywork & Massage Professionals (the professional organization to which I belong) recommended that all therapists suspend operations for at least 4 weeks.</p>
-    <p>I believe it is in the best interest and health of my Clients, their families and myself and my family to remain closed through <strong>June 15</strong>.</p>
-    <p>As information becomes available and it is considered safe to resume operations, please know that your safety is a concern.  Steps have always been taken before each day begins to sanitize the treatment room and this is also done between clients as well as at the end of the day. This practice will continue.</p>
+    <p>I believe it is in the best interest and health of my Clients, their families and myself and my family to remain closed through <strong>July 1</strong>.</p>
     <p>I hope each of you is taking the precautions that are suggested and that you stay well.  I look forward to seeing you on the other side of this challenge.  I am working on additional appropriate safety measures to protect both my valued clients and myself so I am prepared to safely provide services when I reopen.</p>
 </div>
