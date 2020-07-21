881fbd81b5d810c460f5aa0b6d52d119e5b17be9
Scott Hummel <scotthummel@icloud.com>
2020-07-20 19:02:55 -0700

    Last coronavirus update

--- resources/views/partials/coronavirus.blade.php
+++ resources/views/partials/coronavirus.blade.php
@@ -1,6 +1,10 @@
 <div class="coronavirus desktop">
-    <h1>6/9 Update:</h1>
-    <p>At the beginning of the global health crisis, the Arizona Board of Massage Therapy and Associated Bodywork & Massage Professionals (the professional organization to which I belong) recommended that all therapists suspend operations for at least 4 weeks.</p>
-    <p>I believe it is in the best interest and health of my Clients, their families and myself and my family to remain closed through <strong>July 1</strong>.</p>
-    <p>I hope each of you is taking the precautions that are suggested and that you stay well.  I look forward to seeing you on the other side of this challenge.  I am working on additional appropriate safety measures to protect both my valued clients and myself so I am prepared to safely provide services when I reopen.</p>
+    @php $today  =\Carbon\Carbon::now(); @endphp
+    @if ($today->greaterThan(\Carbon\Carbon::create(2020, 07, 23)))
+        <h1>Now Open!</h1>
+        <p>We are now open for business. Please call <a style="color: #FFF; text-decoration: underline;" href="tel:6026950809">(602) 695-0809</a> to schedule an appointment today.</p>
+    @else
+        <h1>Opening Soon!</h1>
+        <p>We will be reopening for business July 23. Please call <a style="color: #FFF; text-decoration: underline;" href="tel:6026950809">(602) 695-0809</a> to schedule an appointment today.</p>
+    @endif
 </div>
